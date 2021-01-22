4d2a5c84ab36cc3a8c86fd7186848bd35f477edc
Marcelo Júnior <rokermarcelo@gmail.com>
2021-01-22 11:00:43 -0300

    Corrigido problema que o método 'exec()' precisava que o método 'isConnected()' fosse chamado antes

--- index.php
+++ index.php
@@ -9,14 +9,18 @@ require_once './model/Conexao.php';
     <body>
         <?php
 //        $conn = new Conexao();
-        echo "Conectado: " . Conexao::isConnected() . "<br>";
-        echo Conexao::getErro();
+//        echo "Conectado: " . Conexao::isConnected() . "<br>";
+//        echo Conexao::getErro();
         echo "<pre>";
-//        $sql1 = "INSERT INTO tecnologia (`nome`) VALUES ('Bootstrap');";
+//        $sql1 = "INSERT INTO tecnologia (`nome`) VALUES ('JQuery');";
         $sql = "SELECT * FROM tecnologia;";
 //        $conn->exec($sql);
         print_r(Conexao::execWithReturn($sql));
-//        Conexao::exec($sql1);
+        echo Conexao::getErro();
+        
+        if(!Conexao::exec($sql1)){
+            echo Conexao::getErro();
+        }
         print_r(Conexao::execWithReturn($sql));
         echo "</pre>";
         ?>
--- model/Conexao.php
+++ model/Conexao.php
@@ -9,6 +9,60 @@ class Conexao {
     private static $erro = "";
     private static $conn = null;
 
+    public static function getErro() {
+        $message = self::$erro;
+        self::$erro = "";
+        return $message;
+    }
+
+    public static function isConnected() {
+        if (self::$conn == null) {
+            return self::connect();
+        }
+        return true;
+    }
+
+    public static function exec($sql) {
+        if (self::isConnected()) {
+            try {
+                return self::$conn->exec($sql);
+            } catch (PDOException $ex) {
+                self::$erro = "Erro ao executar: " . $ex->getMessage();
+            } catch (Exception $ex) {
+                self::$erro = "Erro genérico: " . $ex->getMessage();
+            }
+        }
+        return false;
+    }
+
+    public static function execWithReturn($sql) {
+        if (self::isConnected()) {
+            try {
+                $result = self::$conn->query($sql);
+                return self::fetchResult($result);
+            } catch (PDOException $ex) {
+                self::$erro = "Erro ao consultar: " . $ex->getMessage();
+            } catch (Exception $ex) {
+                self::$erro = "Erro genérico: " . $ex->getMessage();
+            }
+        }
+        return false;
+    }
+    
+    public function __destruct() {
+        self::$conn = null;
+    }
+
+    private static function fetchResult($result){
+        if ($result->rowCount() > 0) {
+            $result->setFetchMode(PDO::FETCH_ASSOC);
+            return $result->fetchAll();
+        } else {
+            self::$erro = "Nenhum registro encontrado!";
+            return false;
+        }
+    }
+    
     private static function connect() {
         try {
             self::$conn = new PDO("mysql:host=" . self::$servername .
@@ -25,50 +79,4 @@ class Conexao {
         }
     }
 
-    public static function getErro() {
-        $message = self::$erro;
-        self::$erro = "";
-        return $message;
-    }
-
-    public static function isConnected() {
-        if (self::$conn == null) {
-            return self::connect();
-        }
-        return true;
-    }
-
-    public static function exec($sql) {
-        try {
-            self::$conn->exec($sql);
-            return true;
-        } catch (PDOException $ex) {
-            self::$erro = "Erro ao executar: " . $ex->getMessage();
-        } catch (Exception $ex) {
-            self::$erro = "Erro genérico: " . $ex->getMessage();
-        }
-        return false;
-    }
-
-    public static function execWithReturn($sql) {
-        if (self::isConnected()) {
-            try {
-                $result = self::$conn->query($sql);
-                if ($result->rowCount() > 0) {
-                    $result->setFetchMode(PDO::FETCH_ASSOC);
-                    return $result->fetchAll();
-                } else {
-                    self::$erro = "Nenhum registro encontrado!";
-                }
-            } catch (PDOException $ex) {
-                self::$erro = "Erro ao consultar: " . $ex->getMessage();
-            } catch (Exception $ex) {
-                self::$erro = "Erro genérico: " . $ex->getMessage();
-            }
-        }
-        return false;
-    }
-    public function __destruct() {
-        self::$conn = null;
-    }    
 }
